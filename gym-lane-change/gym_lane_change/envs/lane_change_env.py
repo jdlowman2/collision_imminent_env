Added scaling to make action space of environment in range [-1, 1]. Fixed scaling issue with arrows on env viewer. Updated tests

gym-lane-change/gym_lane_change/envs/lane_change_env.py
@@ -62,10 +62,7 @@ class LaneChangeEnv(gym.Env):
                                 shape=self.road.get_observation().shape,
                                 dtype=np.float32)
 
-        # IPython.embed()
-        self.action_space = spaces.Box(self.road.vehicle.min_f,
-                                        self.road.vehicle.max_f,(2,),
-                                        dtype=np.float32)
+        self.action_space = spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32)
 
         self.valid_region = {"min_x": -10.0, "max_x": 110.0,
                                 "min_y": -10.0, "max_y": 20.0}
@@ -73,15 +70,23 @@ class LaneChangeEnv(gym.Env):
         self.sparse_reward=sparse_reward
         self.penalty = 0.1
 
-        # self.action_space = spaces.Box(low = np.array([self.road.vehicle.min_f,
-        #                                 self.road.vehicle.min_r]),
-        #                                 high = np.array([self.road.vehicle.max_f,
-        #                                 self.road.vehicle.max_r]),
-        #                                 shape=(2,), dtype=np.float32)
+    def scale_action_to_model(self, action):
+        action[0] = (action[0] + 1)/2.0 * \
+                    (self.road.vehicle.max_f_rate - self.road.vehicle.min_f_rate) +\
+                        self.road.vehicle.min_f_rate
+        action[1] = (action[1] + 1)/2.0 * \
+                    (self.road.vehicle.max_r_rate - self.road.vehicle.min_r_rate) +\
+                        self.road.vehicle.min_r_rate
+
+        return action
 
     def step(self, action):
         self.steps_taken += 1
-        self.road.vehicle.step(action)
+
+        # scale actions
+        scaled_action = self.scale_action_to_model(action)
+
+        self.road.vehicle.step(scaled_action)
 
         self.viewer.last_reward = self.get_reward()
 
@@ -145,12 +150,7 @@ class LaneChangeEnv(gym.Env):
         is_done = False
 
         if self.steps_taken >= self.max_steps or\
-            self.road.is_vehicle_in_collision():# or\
-                # self.road.is_vehicle_in_goal():
-
-            # print("Done because: ", self.steps_taken >= self.max_steps,
-            # self.road.is_vehicle_in_collision(),
-            #     self.road.is_vehicle_in_goal())
+            self.road.is_vehicle_in_collision():
             is_done = True
 
         if self.is_vehicle_outside_valid_region():

gym-lane-change/gym_lane_change/envs/road_model.py
@@ -64,14 +64,10 @@ class Road:
         self.opposing_lane = Lane(length=1000, width=LANE_WIDTH,
                                     x=-100, y=LANE_WIDTH)
 
-        # obs_x =  np.random.uniform(loc=60.0, scale=3.0)
-        # obs_y =  np.random.uniform(loc=0.0, scale=10.0)
-        # obs_w =  np.random.uniform(loc=LANE_WIDTH, scale=0.1*LANE_WIDTH)
-        # obs_l =  np.random.uniform(loc=2.0*LANE_WIDTH, scale=0.1*LANE_WIDTH)
-        obs_x =  60.0
-        obs_y =  0.0
-        obs_w =  0.5*LANE_WIDTH
-        obs_l =  2.0*LANE_WIDTH
+        obs_x =  np.random.uniform(low=55.0,  high=65.0)
+        obs_y =  np.random.uniform(low=-0.1,  high=0.1)
+        obs_w =  np.random.uniform(low=0.45*LANE_WIDTH,  high=0.55*LANE_WIDTH)
+        obs_l =  np.random.uniform(low=1.9*LANE_WIDTH,  high=2.1*LANE_WIDTH)
 
         self.obstacle = Obstacle(length=obs_l, width=obs_w,
                                     x=obs_x, y=obs_y)

gym-lane-change/gym_lane_change/envs/vehicle_model.py
@@ -7,16 +7,22 @@ from rectangle import *
 
 import IPython
 
-MIN_F_STEERING = np.radians(-35.0)
 MAX_F_STEERING = np.radians(35.0)
+MIN_F_STEERING = -MAX_F_STEERING
 
-MIN_R_STEERING = np.radians(-5.0)
-MAX_R_STEERING = np.radians(5.0)
+MAX_R_STEERING = np.radians(10.0)
+MIN_R_STEERING = -MAX_R_STEERING
 
 ## These parameters should change together
 VEHICLE_TIMESTEP = 0.01
 INTEGRATION_STEPS_PER_UPDATE = 10
 ##
+
+MAX_F_STEERING_RATE = np.radians(35.0) # np.radians(70)*VEHICLE_TIMESTEP Paper says 70 degrees per second
+MAX_R_STEERING_RATE = np.radians(10.0) # Paper says 35 degrees per second
+
+MIN_F_STEERING_RATE = -MAX_F_STEERING_RATE
+MIN_R_STEERING_RATE = -MAX_R_STEERING_RATE
 
 def saturate_steering(delta, min_f, max_f, min_r, max_r):
     delta_f, delta_r = delta
@@ -60,6 +66,12 @@ class Vehicle:
         self.max_r = MAX_R_STEERING
         self.min_f = MIN_F_STEERING
         self.min_r = MIN_R_STEERING
+
+        self.max_f_rate = MAX_F_STEERING_RATE
+        self.max_r_rate = MAX_R_STEERING_RATE
+
+        self.min_f_rate = MIN_F_STEERING_RATE
+        self.min_r_rate = MIN_R_STEERING_RATE
 
     def get_rectangle(self):
         return Rectangle(self.length, self.width, self.state.x, self.state.y)

gym-lane-change/gym_lane_change/envs/viewer.py
@@ -91,23 +91,27 @@ class Viewer:
         # self.ax.add_artist(box)
 
     def plot_steering(self):
+        y_scale = 30.0/105.0
         arrow_scale = 5.0
+
         delta_f_norm = np.linalg.norm(self.road.vehicle.state.delta_f)
         delta_r_norm = np.linalg.norm(self.road.vehicle.state.delta_r)
+
         wheel_f = patches.Arrow(
                 x=80.0,
                 y=14.0,
-                dx = arrow_scale*np.cos(-self.road.vehicle.state.delta_f),
-                dy = arrow_scale*np.sin(-self.road.vehicle.state.delta_f),
+                dx = arrow_scale * np.cos(-self.road.vehicle.state.delta_f),
+                dy = arrow_scale * y_scale * np.sin(-self.road.vehicle.state.delta_f),
                 )
         wheel_b = patches.Arrow(
                 x=70.0,
                 y=14.0,
-                dx = arrow_scale*np.cos(-self.road.vehicle.state.delta_r),
-                dy = arrow_scale*np.sin(-self.road.vehicle.state.delta_r),
+                dx = arrow_scale * np.cos(-self.road.vehicle.state.delta_r),
+                dy = arrow_scale * y_scale * np.sin(-self.road.vehicle.state.delta_r),
                 )
         self.ax.text(x=80.0, y=16.0, s="Front\nSteer", color="black")
         self.ax.text(x=70.0, y=16.0, s="Rear\nSteer", color="black")
+
         self.ax.add_patch(wheel_f)
         self.ax.add_patch(wheel_b)
 

gym-lane-change/gym_lane_change/tests/demo_env.py
@@ -26,22 +26,24 @@ def show_setup():
     env.close()
 
 
-def demonstrate_action(action="random", sparse=False):
+def demonstrate_action(test_action="random", sparse=False):
     env = LaneChangeEnv(sparse)
     s = env.reset()
     done = False
     total_reward = 0.0
+    # IPython.embed()
 
     while not done:
         env.render()
-        if type(action)==str and action.lower() == "random":
+        if type(test_action)==str and test_action.lower() == "random":
             a = env.action_space.sample()
         else:
-            a = action
+            a = test_action.copy()
 
         s, r, done, info = env.step(a) # random action
         total_reward += r
         print("Total reward: ", total_reward)
+        print("Test Action: ", test_action)
     env.close()
 
     return total_reward
@@ -171,17 +173,30 @@ if __name__ == "__main__":
 
     sparse = args.sparse
     env = LaneChangeEnv()
+    low, high = env.action_space.low, env.action_space.high
 
     print(f"Demonstrating with sparse? {sparse}")
 
     print("Demonstrating random action...")
+    time.sleep(0.9)
     demonstrate_action("random", sparse)
 
     print("Demonstrating zero turning...")
-    demonstrate_action(0*env.action_space.low, sparse)
+    time.sleep(0.9)
+    demonstrate_action(0*low, sparse)
 
-    print("Demonstrating low action...")
-    demonstrate_action(env.action_space.low, sparse)
+    print("Demonstrating low action...", env.action_space.low)
+    time.sleep(0.9)
+    demonstrate_action(low, sparse)
 
     print("Demonstrating high action...")
-    demonstrate_action(env.action_space.high, sparse)
+    time.sleep(0.9)
+    demonstrate_action(high, sparse=sparse)
+
+    print("Demonstrating front wheel only action...")
+    time.sleep(0.9)
+    demonstrate_action(np.array([high[0], 0.0]), sparse=sparse)
+
+    print("Demonstrating front wheel only action...")
+    time.sleep(0.9)
+    demonstrate_action(np.array([low[0], 0.0]), sparse=sparse)

gym-lane-change/gym_lane_change/tests/vehicle_model_test.py
@@ -54,14 +54,14 @@ def compute_dx(si, vehicle, dt, test_input):
 class TestVehicle(unittest.TestCase):
 
     def test_no_input(self):
-        vehicle = make_dummy_vehicle(0.0, 0.0, 0.0)
-        for i in range(2):
+        vehicle = make_dummy_vehicle(0.0, 0.0, 35.0)
+        for i in range(10):
             vehicle.step([0.0, 0.0])
-            print("Vehicle state: ")
-            print(vehicle.state)
-            self.assertTrue(vehicle.state, State(0.0, 0.0, 0.0,
-                                                 0.0, 0.0, 0.0,
-                                                 0.0, 0.0))
+            # print(vehicle.state)
+            self.assertTrue(vehicle.state.psi == 0.0)
+            self.assertTrue(vehicle.state.u == 35.0)
+            self.assertTrue(vehicle.state.delta_f == 0.0)
+            self.assertTrue(vehicle.state.delta_r == 0.0)
 
     def test_saturate_steering(self):
         f, r = saturate_steering([MAX_F_STEERING, MAX_R_STEERING],
@@ -255,16 +255,14 @@ def plot_sigma(step):
     plt.tight_layout()
     # plt.show()
 
-# step = np.array([1.0, 0.0])
-step = np.array([np.radians(30).round(4), 0.0])
-plot_sigma(step)
-plot_action(step)
-plot_steering(step)
-plot_lateral_forces(step)
-plot_vehicle(step)
-# plot_vehicle_omega(step)
-# plot_vehicle_psi(step)
-plt.show()
+def plot_tests():
+    step = np.array([np.radians(30).round(4), 0.0])
+    plot_sigma(step)
+    plot_action(step)
+    plot_steering(step)
+    plot_lateral_forces(step)
+    plot_vehicle(step)
+    plt.show()
 
 if __name__ == '__main__':
     unittest.main()